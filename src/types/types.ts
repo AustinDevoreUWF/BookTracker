800db735124487ee3047edc144d55005f1a2c149
AustinDevoreUWF <ard78@students.uwf.edu>
2026-07-16 18:16:12 -0500

    Initialized

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+type User = {
+    id: number,
+    isAdmin: boolean,
+    name: string,
+    email: string,
+    password: string,
+}
+type Book = {
+    id: number,
+    name: string,
+    author: string,
+    pubDate: Date,
+    pageCount: number,
+    
+}
